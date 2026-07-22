24b52f5f8d2b088bef86e042b7573b4a2dc455ec
leonardosecchin <secchinleo@gmail.com>
2026-07-22 12:32:58 -0300

    automated push

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,6 +15,8 @@ using SparseArrays
     A = Symmetric([1.0 0.5 0.1; 0.0 2.0 0.0; 0.0 0.0 3.0])
     b = A * ones(3)
 
+    p = 2.0
+
     # function to allocate and initialize data
     function data_initialize(x, bs)
         data = DATA(
@@ -37,24 +39,33 @@ using SparseArrays
     # f(x) = 1/p |Ax - b|_p^p
     # i = 0 indicates that data.Axb is up to date, so we compute the p-norm
     # directly
-    function f(s, bs, i, data)
+    function f(x, s, bs, i, data)
         if (i > 0)
             # compute Ai * s
             @inbounds data.Asi .= data.A[i] * s
             # update A*x - b
             @. data.Axb += data.Asi
         end
-        return 0.5 * norm(data.Axb, 2)^2
+        return (1/p) * norm(data.Axb, p)^p
     end
 
     # partial gradient
-    function g!(g, bs, i, data)
-        @inbounds @views g[bs[i].idx] .= transpose(data.A[i]) * data.Axb
+    function g!(g, x, bs, i, data)
+        @inbounds @views if p == 2.0
+            g[bs[i].idx] .= transpose(data.A[i]) * data.Axb
+        else
+            g[bs[i].idx] .= transpose(data.A[i]) * (abs.(data.Axb).^(p-1) .* sign.(data.Axb))
+        end
     end
 
     # update B_i
-    function B(bs, i, data)
-        return @inbounds transpose(data.A[i]) * data.A[i]
+    function B(x, bs, i, data)
+        @inbounds ni = bs[i].ni
+        @inbounds if p == 2.0
+            return transpose(data.A[i]) * data.A[i]
+        else
+            return (p-1)*transpose(data.A[i]) * spdiagm(min.(abs.(data.Axb).^(p-2), D)) * data.A[i]
+        end
     end
 
     blocks = create_blocks(3, [1;2;3])
